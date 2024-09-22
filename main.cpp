80f05d929a1bec0272bf2486dd63ff0fc8c3429a
Rodrigo <rodrigo.rcastro27@gmail.com>
2024-09-22 13:02:54 -0300

    initial version

--- main.cpp
+++ main.cpp
@@ -13,81 +13,109 @@ struct state{
     bool is_accepting;  
 };
 
-class Automaton {
-    public:
-        vector<state> states;                    
-        unordered_map<int, vector<transition>> transition_table; 
+vector<char> symbols;                    
+vector<string> input_strings;
+unordered_map<int, vector<transition>> transition_table; 
 
-        void add_state(int id, bool is_accepting) {
-            states.push_back({id, is_accepting});
+vector<int> find_paths(char letter, int index){
+    vector<int> paths;
+
+    for(auto transition : transition_table[index]){
+        if(letter == transition.symbol || letter == '-')
+            paths.push_back(transition.next_state);
+    }
+
+    return paths;
+}
+
+bool is_terminal_symbol(char symbol){
+    for(auto c: symbols){
+        if(c == symbol)
+            return true;
+    }
+
+    return false;
+}
+
+// backtracking
+bool process_string(vector<state> states, string input, string currtn, int indx_s, int indx_i) {
+    if(states[indx_s].is_accepting && currtn == input && is_terminal_symbol(currtn[indx_i])){
+        return true;
+    }else{
+        vector<int> paths = find_paths(input[indx_i], indx_s);
+
+        for(auto path : paths){
+            if(process_string(states, input, currtn + input[indx_i], path, indx_i + 1))
+                return true;
         }
+    }
 
-        void add_transition(int from_state, char symbol, int to_state) {
-            transition_table[from_state].push_back({symbol, to_state});
-        }
-
-        bool process_string(string input) {
-           // Backtracking ....
-        }
-};
-
-// Function to tranform qi --> i
-int string_to_number(string str) {
-    if(str.empty() || str[0] != 'q')
-        throw invalid_argument("Invalid");
-    
-    return stoi(str.substr(1));
+    return false;
 }
 
 int main(){
-    Automaton automaton_x;
 
     // First Line: number of states
     int n_states;
     cin >> n_states;
+    vector<state> states(n_states);
+
+    for(int i = 0; i<n_states; i++){
+        states[i].id = i;
+        states[i].is_accepting = false;
+    }
 
     // Second Line: terminal symbols 
     int n_symbols;
     cin >> n_symbols;
 
-    vector<char> symbols(n_symbols);
-    for(int i = 0; i < n_symbols; i++)
-        cin >> symbols[i];
-
+    for(int i = 0; i < n_symbols; i++){
+        char buffer_char;
+        cin >> buffer_char;
+        symbols.push_back(buffer_char);
+    }
+   
     // Third Line: initial states
     int n_initial_states;
     cin >> n_initial_states;
-
-    vector<int> initial_states(n_initial_states);
-    for(int i = 0; i < n_initial_states; i++)
-        cin >> initial_states[i];
 
     // Fourth Line: acceptance states
     int n_acceptance_states;
     cin >> n_acceptance_states;
 
-    vector<int> acceptance_states(n_acceptance_states);
-    for(int i = 0; i < n_acceptance_states; i++)
-        cin >> acceptance_states[i];
+    for(int i = 0; i < n_acceptance_states; i++){
+        int buffer_int;
+        cin >> buffer_int;
+        states[buffer_int].is_accepting = true;
+    }
 
     // Fifth Line: number of transitions
     int n_transitions;
     cin >> n_transitions;
 
-    // Sixth Line: the transitions
+    // Nexts N Lines: read the transitions
     for(int i = 0; i < n_transitions; i++){
-        string q_initial, q_final, x;
+        int from_state, to_state;
+        char symbol;
 
-        cin >> q_initial >> x >> q_final;
+        cin>> from_state >> symbol >> to_state;
+        transition_table[from_state].push_back({symbol, to_state});
     }
 
     // Inputs
     int n_input_strings;
     cin >> n_input_strings;
 
-    vector<string> input_strings(n_input_strings);
-    for(int i = 0; i < n_input_strings; i++)
-        cin >> input_strings[i];
+    vector<string> input_strings;
+    for(int i = 0; i < n_input_strings; i++){
+        string buffer_str;
+        cin >> buffer_str;
+        input_strings.push_back(buffer_str);
+    }
+        
+    for(auto input: input_strings){
+        (process_string(states, input, "", 0, 0))? cout << "aceita\n" : cout << "rejeita\n";
+    }
 
     return 0;
 }
